65259b43546e68120399645072a9ce0e08f4297b
Kim-NeuralRiver <kim@neuralriver.ai>
2025-05-19 13:00:27 +0100

    Added ITAD service to handle game IDs and fetch game prices

--- pixelplaylist/settings.py
+++ pixelplaylist/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+# Load environment variables from .env file
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
