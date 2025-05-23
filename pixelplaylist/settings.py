1231ce18f682ca3cfdef3a9f056903ff30136fa0
Kim-NeuralRiver <kim@neuralriver.ai>
2025-05-23 17:06:51 +0100

    Migrated to NeonDB

--- pixelplaylist/settings.py
+++ pixelplaylist/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 from pathlib import Path
+import dj_database_url
 
 # Load environment variables from .env file
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
+    'recommendations'
 ]
 
 MIDDLEWARE = [
@@ -88,10 +90,7 @@ WSGI_APPLICATION = 'pixelplaylist.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
 }
 
 
--- recommendations/urls.py
+++ recommendations/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import GameRecommendationView, GenreListView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    GameRecommendationView,
+    GenreListView,
+    GamePlaylistListCreate,
+)
 
 urlpatterns = [
-    path('recommendations/', GameRecommendationView.as_view(), name='recommendations'),
+    path('recommendations/', GameRecommendationView.as_view(), name='game-recommendations'),
     path('genres/', GenreListView.as_view(), name='genre-list'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Add more paths if / when needed
+    path('playlists/', GamePlaylistListCreate.as_view(), name='playlist-list-create'),
 ]
