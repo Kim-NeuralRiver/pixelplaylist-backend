5451acb2ffae5fdd40b2c568690c4f066b46c7d3
Kim-NeuralRiver <kim@neuralriver.ai>
2025-05-27 11:30:17 +0100

    Populated README and created a requirements.txt. Also updated .env.example and altered settings.py

--- pixelplaylist/settings.py
+++ pixelplaylist/settings.py
@@ -90,7 +90,10 @@ WSGI_APPLICATION = 'pixelplaylist.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
+    "default": dj_database_url.parse(
+        url=os.getenv("DATABASE_URL", ""),
+        conn_max_age=600, conn_health_checks=True
+    )
 }
 
 
