3da07a7499847b3ad0ae3513d03ef315374773d0
Kim-NeuralRiver <kim@neuralriver.ai>
2025-06-05 14:56:23 +0100

    Debug JWT auth error & swagger ui static error fix
    Fix Swagger UI and static file logic issue, causing Swagger UI not to show.
    
    Continue attempt to fix JWT auth errors. Pushing to make available for potential code review.

--- recommendations/serializers.py
+++ recommendations/serializers.py
@@ -1,9 +1,28 @@
 # Defines how data from the GamePlaylist model is serialized into JSON for the frontend, double checking this today
 from rest_framework import serializers
 from .models import GamePlaylist
+from django.contrib.auth.models import User
 
 class GamePlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = GamePlaylist
         fields = ['id', 'name', 'games', 'created_at', 'user']
         read_only_fields = ['id']
+        
+
+# User create serializer using Django's built in user model 
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        
+        return user
